c983f114c7a66fed71ba9939db724bd3a49783d3
Mehdi Hmidi <mh223vk@student.lnu.se>
2017-10-27 10:26:22 +0200

    :congratulations: :8ball:

--- src/TechnicalServices/Search/NamePrefixCriteria.java
+++ src/TechnicalServices/Search/NamePrefixCriteria.java
@@ -5,22 +5,16 @@ import Model.MemberRegistry;
 
 public class NamePrefixCriteria implements SearchCriteria {
 	
-		private String namePrefix;
-		
-		public NamePrefixCriteria(String namePrefix){
+	private String namePrefix;
+	public NamePrefixCriteria(String namePrefix){
 			this.namePrefix = namePrefix;
 		}
-	
-		@Override
-	   public MemberRegistry meetCriteria(MemberRegistry members) {
-	      MemberRegistry filter_result = new MemberRegistry(); 
-	      
-	      for (Member m : members.getMemberList()) {
-	    	  if (m.getM_name().startsWith(namePrefix)){
-	            filter_result.addMember(m);
-	         }
-	      }
+
+	@Override
+	public MemberRegistry meetCriteria(MemberRegistry members) {
+		MemberRegistry filter_result = new MemberRegistry();
+		for (Member m : members.getMemberList())
+			if (m.getM_name().startsWith(namePrefix)){ filter_result.addMember(m); }
 	      return filter_result;
 	   }
-
 }
